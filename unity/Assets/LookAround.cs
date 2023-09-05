22107d4eb11654fba5001b5905639639bef6de21
afaysalk <afaysalkhaldi@gmail.com>
2023-09-05 19:07:05 -0400

    Look around script

---
+++ unity/Assets/LookAround.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public GameObject mainCam;
+    public Transform pivot;
+    public float speed;
+    bool rotating;
+    float rotated;
+
+    void Update()
+    {
+        if (!rotating && Input.GetKeyDown(KeyCode.L))
+        {
+            StartRotation();
+        }
+
+        if (rotating)
+        {
+            Rotate();
+        }
+    }
+
+    private void StartRotation()
+    {
+        rotating = true;
+        rotated = 0;
+        mainCam.SetActive(false);
+    }
+
+    private void Rotate()
+    {
+        rotated += speed * Time.deltaTime;
+        var angles = pivot.eulerAngles;
+        angles.y = rotated;
+        if (rotated >= 360)
+        {
+            angles.y = 0;
+            rotating = false;
+            mainCam.SetActive(true);
+        }
+
+        pivot.eulerAngles = angles;
+    }
+}
